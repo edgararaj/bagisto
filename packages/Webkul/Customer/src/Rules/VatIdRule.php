3e99ace2e2a1c132c47506d024fc3ada5d287d6a
Edgar Araújo <edgararaj@gmail.com>
2023-05-10 17:09:57 +0100

    novo vatidrule

--- packages/Webkul/Customer/src/Rules/VatIdRule.php
+++ packages/Webkul/Customer/src/Rules/VatIdRule.php
@@ -3,7 +3,6 @@
 namespace Webkul\Customer\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
-use Webkul\Customer\Rules\VatValidator;
 
 /**
  * Class VatIdRule
@@ -13,6 +12,41 @@ use Webkul\Customer\Rules\VatValidator;
  */
 class VatIdRule implements Rule
 {
+    private function validaNIF($nif, $ignoreFirst=true) {
+        //Limpamos eventuais espaços a mais
+        $nif=trim($nif);
+        //Verificamos se é numérico e tem comprimento 9
+        if (!is_numeric($nif) || strlen($nif)!=9) {
+            return false;
+        } else {
+            $nifSplit=str_split($nif);
+            //O primeiro digíto tem de ser 1, 2, 3, 5, 6, 8 ou 9
+            //Ou não, se optarmos por ignorar esta "regra"
+            if (
+                in_array($nifSplit[0], array(1, 2, 3, 5, 6, 8, 9))
+                ||
+                $ignoreFirst
+            ) {
+                //Calculamos o dígito de controlo
+                $checkDigit=0;
+                for($i=0; $i<8; $i++) {
+                    $checkDigit+=$nifSplit[$i]*(10-$i-1);
+                }
+                $checkDigit=11-($checkDigit % 11);
+                //Se der 10 então o dígito de controlo tem de ser 0
+                if($checkDigit>=10) $checkDigit=0;
+                //Comparamos com o último dígito
+                if ($checkDigit==$nifSplit[8]) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+
     /**
      * Determine if the validation rule passes.
      *
@@ -25,9 +59,7 @@ class VatIdRule implements Rule
      */
     public function passes($attribute, $value)
     {
-        $validator = new VatValidator();
-
-        return empty($value) || $validator->validate($value);
+        return empty($value) || $this->validaNIF($value);
     }
 
     /**
